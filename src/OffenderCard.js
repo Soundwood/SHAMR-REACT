9b83e639b893d0e98ca569b55680268644c1e189
Soundwood <j.benhogan@gmail.com>
2020-11-24 16:53:48 -0600

    add offenses to offender card

--- src/OffenderCard.js
+++ src/OffenderCard.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const OffenderCard = (props) => {
     const {offender} = props
+    const offenses = offender.offense_categories.map((element) => <p key={element.id}> - {element.name}</p>)
     return (
         <div className="w3-col s6 w3-center">
             <h5>{offender.display_name} - <a href={`https://twitter.com/${offender.username}`}>@{offender.username}</a></h5>
+                {offenses}
             <button onClick={() => {props.handleDeleteOffender(props.offender.id)}}>Remove from Offenders</button>
         </div>
     );
